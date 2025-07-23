7a084927b2d16adb49d8edf551a4c8f587b92a3d
PulinduVR <pulinduv@gmail.com>
2025-07-24 01:49:58 +0530

    feat(navigation): Web Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import { Navigation } from './components/ui/Navigation';
 import React from 'react';
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome to LeafScan</h1>
-      </div>
+      <Navigation / >
     </>
   )
 }
---
+++ src/components/ui/Navigation.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/leafScan.png';
+
+export const Navigation = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-100">
+        <div className='container mx-auto px-6'>
+            <div className="flex items-center justify-between">
+                <Link to="/" className="flex items-center space-x-3">
+                    <div className='rounded-lg'>
+                        <img src={logo} alt="LeafScan Logo" className="w-12 h-12 mt-5" />'
+                    </div>
+                    <span className="text-2xl font-bold text-green-800">
+                        LeafScan
+                    </span>
+                </Link>
+
+                <div className='flex items-center space-x-6'> 
+                    <Link to="/login" className="text-gray-700 hover:text-green-700 font-medium">
+                        Login
+                    </Link>
+                    <Link to="/register">
+                        <button className="bg-green-800 hover:bg-green-600 text-white px-6 py-2 rounded-lg font-medium transition-colors cursor-pointer">
+                            Get Started
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </nav>
+  );
+}
